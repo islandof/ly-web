e2d9f6137817f9b70a5aaa982c2660cf8200aa57
islandof <islandof@foxmail.com>
2018-02-02 21:02:50 +0800

    init

--- public/assets/libs/layui/lay/modules/layim.js
+++ public/assets/libs/layui/lay/modules/layim.js
@@ -618,6 +618,7 @@ layui.define(["layer", "laytpl", "upload"], function(i) {
 				var n = e.find("li").eq(0);
 				e.prev().hasClass("layim-chat-system") || e.before('<div class="layim-chat-system"><span layim-event="chatLog">查看更多记录</span></div>'), t > d && n.remove()
 			}
+			//不自动滚动
 			// a.scrollTop(a[0].scrollHeight + 1e3), a.find("ul li:last").find("img").load(function() {
 			// 	a.scrollTop(a[0].scrollHeight + 1e3)
 			// })
